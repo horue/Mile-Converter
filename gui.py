619886290d7c97d63b9498a3755ce29ed3c064bc
Jorge <jorgejapao00@gmail.com>
2024-04-21 20:59:46 -0300

    Now the converted value shows ins screen. Showing multiple results.

--- gui.py
+++ gui.py
@@ -1,21 +1,25 @@
 import tkinter as tk
+from tkinter import *
 
 
-def convert(e1):
+def convert(root, e1):
     miles = e1.get()
     final = float(miles) * 1.6
     print(final)
 
+    l1 = tk.Label(root, text=final)
+    l1.pack_forget
+    l1.pack()
 
 def m(root):
     e1 = tk.Entry(root)
     e1.pack()
 
-    b1 = tk.Button(root, text='Convert', command=lambda:convert(e1))
+    b1 = tk.Button(root, text='Convert', command=lambda:convert(root, e1))
     b1.pack()
 
-    l1 = tk.Label(root)
-    l1.pack()
+
+
 
 
 def main():
