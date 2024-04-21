451179d680ff857a1610905739c646721191b32d
Jorge <jorgejapao00@gmail.com>
2024-04-21 20:12:18 -0300

    Gui created

--- gui.py
+++ gui.py
@@ -1,1 +1,20 @@
 import tkinter as tk
+
+
+def m(root):
+    pass
+
+
+def main():
+    root = tk.Tk()
+    root.title("Mile Converter")
+    root.resizable(True, True)
+                
+    m(root)
+
+    root.iconify()
+    root.update()
+    root.deiconify()
+    root.mainloop()
+
+main()
